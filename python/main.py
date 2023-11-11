14d3b3d04aee4aeb1583872f0131ff1868bd89bb
mariollesta <llesta.mario@gmail.com>
2023-11-11 21:39:01 +0100

    [my-own-wc]: Final Step

--- python/main.py
+++ python/main.py
@@ -1,4 +1,5 @@
 import typer, chardet
+import sys
 from rich import print
 
 
@@ -19,50 +20,55 @@ def mywc(
     With no FILE, read standard input.
     """
     try:
-        with open(file, 'rb') as f:
-            content = f.read()
+        
+        if file:
+            with open(file, 'rb') as f:
+                content = f.read()
+        else:
+            content = sys.stdin.buffer.read()
+                
+             
+        if c:
+            bytes = len(content)
+            print(f"{bytes} {file}")
             
-            if c:
-                bytes = len(content)
-                print(f"{bytes} {file}")
-                
-            if l:
-                lines = content.count(b'\n')
-                print(f"{lines} {file}")
-                
-            if w: 
-                words = content.split()
-                print(f"{len(words)} {file}")
-                
-            if m:
-                encoding_result = chardet.detect(content)
-                encoding = encoding_result['encoding']
-                
-                try:
-                    with open(file, 'r', encoding=encoding) as tmp: 
-                        line_breaks = 0
-                        characters_per_line = 0
-                        total_characters = 0
-                        
-                        lines = tmp.readlines()
-                        line_breaks = len(lines)
-                        
-                        for line in lines:
-                           characters_per_line += len(line)
-                        
-                        total_characters = characters_per_line + line_breaks + 1
-                        
-                        print(f"{total_characters} {file}")
+        elif l:
+            lines = content.count(b'\n')
+            print(f"{lines} {file}")
+            
+        elif w: 
+            words = content.split()
+            print(f"{len(words)} {file}")
+            
+        elif m:
+            encoding_result = chardet.detect(content)
+            encoding = encoding_result['encoding']
+            
+            try:
+                with open(file, 'r', encoding=encoding) as tmp: 
+                    line_breaks = 0
+                    characters_per_line = 0
+                    total_characters = 0
                     
-                except Exception as e:
-                    print(f"Error: {e}")    
-            
-            else:
-                lines = content.count(b'\n')
-                words = len(content.split())
-                bytes = len(content)
-                print(f"{lines} {words} {bytes} {file}")        
-    
+                    lines = tmp.readlines()
+                    line_breaks = len(lines)
+                    
+                    for line in lines:
+                        characters_per_line += len(line)
+                    
+                    total_characters = characters_per_line + line_breaks + 1
+                    
+                    print(f"{total_characters} {file}")
+                
+            except Exception as e:
+                print(f"Error: {e}")    
+        
+        else:
+            lines = content.count(b'\n')
+            words = len(content.split())
+            bytes = len(content)
+            print(f"{lines} {words} {bytes} {file}")
+
     except FileNotFoundError:
         print(f"Error: File not found.")
     
